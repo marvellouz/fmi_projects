5fe09797c69aff8bba91247cb123295b682c86f7
marvellouz <vdinkova@fmi.uni-sofia.bg>
2011-01-05 15:19:21 +0200

    dot net task 3 compareNormal

--- FMIdotNOTandWTF/Task3/Task3/Program.cs
+++ FMIdotNOTandWTF/Task3/Task3/Program.cs
@@ -21,6 +21,56 @@ namespace Task3
 
     public class XmlComparer
     {
+        public static bool compareNodes(XmlNode node1, XmlNode node2)
+        {
+            if (node1.InnerText == node2.InnerText)
+            {
+                if ((node1.Name == node2.Name) &&
+                    (node1.Attributes.Count == node2.Attributes.Count))
+                {
+                    if (node1.Attributes.Count != 0)
+                    {
+                        for (int i = 1; i < node1.Attributes.Count; i++)
+                        {
+                            if (node1.Attributes[i].Name != node2.Attributes[i].Name)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool compareNormal(XmlNode rootNode1, XmlNode rootNode2)
+        {
+            if (!compareNodes(rootNode1, rootNode2))
+            {
+                return false;
+            }
+            
+            if (rootNode1.ChildNodes.Count != rootNode2.ChildNodes.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < rootNode1.ChildNodes.Count; i++)
+            {
+                if (!compareNormal(rootNode1.ChildNodes[i], rootNode2.ChildNodes[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
         public static bool Compare(string xml1, string xml2, CompareOptions options)
         {
             XmlDocument xdoc1 = new XmlDocument();
@@ -47,24 +97,6 @@ namespace Task3
                 XmlDocument xdoc2Lower = new XmlDocument();
                 xdoc2Lower.LoadXml(xml2Lower);
                 XmlNode rootNode2Lower = xdoc2Lower.DocumentElement;
-
-                if (rootNode1Lower.InnerText == rootNode2Lower.InnerText)
-                {
-                    if ((rootNode1Lower.Name == rootNode2Lower.Name) &&
-                        (rootNode1Lower.Attributes.Count == rootNode2Lower.Attributes.Count))
-                    {
-                        if (rootNode1Lower.Attributes.Count != 0)
-                        {
-                            for (int i = 1; i < rootNode1Lower.Attributes.Count; i++)
-                            {
-                                if (rootNode1Lower.Attributes[i].Name == rootNode2Lower.Attributes[i].Name)
-                                {
-                                    Console.WriteLine(rootNode2Lower.Attributes[i].Name);
-                                }
-                            }
-                        }
-                    }
-                }
             }
 
             return false;
