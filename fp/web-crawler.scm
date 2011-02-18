05ba8030ec39df096128d5e7c91be4d314e858e9
marvellouz <vdinkova@fmi.uni-sofia.bg>
2011-02-18 13:31:47 +0200

    crawler saveing

--- fp/web-crawler.scm
+++ fp/web-crawler.scm
@@ -15,13 +15,9 @@
 ;    (get-pure-port (string->url url))
 ;  ))
 
-(define (validate start-url depth save-location)
-  (if (or (null? start-url) (null? depth) (null? save-location)) (list #f "Моля попълнете всички полета")
-      (if (directory-exists? (string->path save-location))(list #t)
-          (list #f "Невалиден път"))))
 
-(define (error e)
-  )
+
+;(define (error e))
 
 (define (base-url u)
   ;"http://someurl.com/images/foo/bar..." -> http://someurl.com/
@@ -38,7 +34,7 @@
   (define rx (regexp "href=\"([^>\"]*)\""))
   (define res (regexp-match* rx (get-html current-url)))
   (define (helper str) (regexp-replace rx str "\\1"))
-  (map (lambda (str) (bytes->string/utf-8 str)) (map helper res))
+  (map url->string (map (lambda (x) (normalize-resources (string->url current-url) x)) (map (lambda (str) (bytes->string/utf-8 str)) (map helper res))))
   )
 
 ;list of all srcs
@@ -46,50 +42,76 @@
   (define rx (regexp "src=\"([^>\"]+)\""))
   (define res (regexp-match* rx (get-html current-url)))
   (define (helper str) (regexp-replace rx str "\\1"))
-  (map (lambda (str) (bytes->string/utf-8 str)) (map helper res))
+  (map url->string (map (lambda (x) (normalize-resources (string->url current-url) x)) (map (lambda (str) (bytes->string/utf-8 str)) (map helper res))))
   )
 
 ;========================================================================================
 
 (define (get-html u)
-    (get-pure-port (string->url u)))
+  (get-pure-port (string->url u)))
 
 (define (read-and-write in out)
   (let ((res (read-bytes 512 in)))
     (if (not (eof-object? res))
-      (begin
-        (write-bytes res out)
-        (read-and-write in out))
-      out)))
+        (begin
+          (write-bytes res out)
+          (read-and-write in out))
+        out)))
 
-;save-location must include the dile name. For example "D:\\FMI\\fmi_projects\\fp\\picture.jpg"
-(define (save! current-url save-location)
+;save-location "D:\\FMI\\fmi_projects\\fp\\"
+;to add "http://a.b/c.jpg" -> "D:\\FMI\\fmi_projects\\fp\\src\\53413520368201237500034"
+;dos save-location exist?
+(define (save! current-url save-location save-sublocation)
   (define in (get-html current-url))
-  (define out (open-output-file (string->path save-location)))
+  (define out (open-output-file (string->path (calculate-local-location current-url save-location save-sublocation))))
   (read-and-write in out)
   (close-output-port out))
 
 ;=========================================================================================
 
-(define (crawl current-url depth save-location)
-  (let (resources (normalize-resources (find-all-resources current-url)))
-    (begin
-      (save-html! current-url save-location file-name)
-      (save-resources! resources))))
-  
-(define (calculate-local-location res save-location) 
-  
-  )
-  
-(define (save-resources! resources save-location)
-  (let (res (car resources)))
-  (begin
-    (save! res (calculate-local-location res save-location))
-    (save-resources! (cdr resources) save-location))
-  )
+(define (crawl start-url depth save-location)
+    (if (= 0 depth) #f
+      (let (
+            (hrefs (find-all-hrefs current-url))
+            (src (find-all-src current-url))
+            )
+        (begin
+          (save! start-url save-location "")
+          (save-resources! hrefs save-location "hrefs\\")
+          (save-resources! src save-location "src\\")
+          (crawl-all (car hrefs) (-depth 1) save-location)
+          )))
 
-(define (main start-url depth save-location)
-  (let (valid (validate start-url depth save-location file-name)))
-  (if (car valid)
-      (crawl start-url depth save-location)
-      (error (cdr valid))))
+(define (crawl-all current-url depth save-location)
+  (if (= 0 depth) #f
+      (let (
+            (hrefs (find-all-hrefs current-url))
+            (src (find-all-src current-url))
+            )
+        (begin
+          (save! current-url save-location "hrefs\\")
+          (save-resources! hrefs save-location "hrefs\\")
+          (save-resources! src save-location "src\\")
+          (for-each (lambda (x) (crawl-all x (- depth 1) save-location))
+                    (find-all-hrefs current-url)
+                    ))))
+  
+  (define (calculate-local-location res save-location save-sublocation) 
+    (string-append save-location save-sublocation (bytes->string/utf-8 (md5 res))))
+  
+  (define (save-resources! resources save-location save-sublocation)
+    (let (res (car resources)))
+    (begin
+      (save! res (calculate-local-location res save-location))
+      (save-resources! (cdr resources) save-location)))
+  
+  (define (validate start-url depth save-location)
+    (if (or (null? start-url) (null? depth) (null? save-location)) (list #f "Моля попълнете всички полета")
+        (if (directory-exists? (string->path save-location))(list #t)
+            (list #f "Невалиден път"))))
+  
+  (define (main start-url depth save-location)
+    (let (valid (validate start-url depth save-location file-name)))
+    (if (car valid)
+        (crawl start-url depth save-location)
+        (error (cdr valid))))
