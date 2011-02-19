435ccd457bcaf32fa8a0218bb340e25f953a4e7c
marvellouz <vdinkova@fmi.uni-sofia.bg>
2011-02-19 03:32:56 +0200

    invalid urls

--- fp/web-crawler.rkt
+++ fp/web-crawler.rkt
@@ -33,7 +33,7 @@
 ;(url->string (combine-url/relative (string->url "http://asd.ds/x/y/z") "../a/b/c"))
 ;"http://asd.ds/x/a/b/c"
 (define (normalize-resources current-url resource)
-  (combine-url/relative current-url resource))
+  (string->url (regexp-replace "#.*" (url->string (combine-url/relative current-url resource)) "" )))
 
 ;list of all hrefs
 (define (find-all-hrefs current-url)
@@ -64,14 +64,11 @@
           (read-and-write in out))
         out)))
 
-;save-location "D:\\FMI\\fmi_projects\\fp\\"
-;to add "http://a.b/c.jpg" -> "D:\\FMI\\fmi_projects\\fp\\src\\53413520368201237500034"
-;dos save-location exist?
-(define (save! current-url save-location save-sublocation)
+(define (save! current-url save-location )
   (define in (get-html current-url))
-  (define output-file (string->path (calculate-local-location current-url save-location save-sublocation)))
+  (define output-file (string->path (calculate-local-location current-url save-location )))
   (and (not (directory-exists? (directory-part output-file)))
-      (make-directory* (directory-part output-file)))
+       (make-directory* (directory-part output-file)))
   (define out (open-output-file output-file))
   (read-and-write in out)
   (close-output-port out))
@@ -86,16 +83,17 @@
             (start-page (make-page start-url hrefs src null))
             )
         (begin
-          (save! start-url save-location null)
+          (set! visited-hrefs (set-add visited-hrefs start-url))
+          (save! start-url save-location)
+          (display (length src))
           (and (not (null? hrefs))
-               (save-resources! hrefs save-location "hrefs\\"))
+               (save-resources! hrefs save-location))
+          
           (and (not(null? src))
-               (save-resources! src save-location "src\\"))
-          (set! visited-hrefs(set-add visited-hrefs start-url))
+               (save-resources! src save-location))
+          (display (length src))
           (for-each (lambda (x) (crawl-all (- depth 1) save-location (make-page x (find-all-hrefs x) (find-all-src x) start-page)))
-                    hrefs)
-          ;(crawl-all (car hrefs) (-depth 1) save-location)
-          ))))
+                    hrefs)))))
 
 (define (crawl-all depth save-location current-page)
   (if (= 0 depth) #f
@@ -110,40 +108,43 @@
         (and 
          (not(set-member? visited-hrefs current-url))
          (begin
-           (save! current-url save-location "hrefs\\")
-           (save-resources! hrefs save-location "hrefs\\")
-           (save-resources! src save-location "src\\")
+           (save! current-url save-location)
+           (save-resources! hrefs save-location)
+           (save-resources! src save-location)
            (set! visited-hrefs(set-add visited-hrefs current-url))
+           (display (set-count visited-hrefs))
            (for-each (lambda (x) (crawl-all (- depth 1) save-location)(make-page x (find-all-hrefs x) (find-all-src x) current-page))
                      hrefs))))))
 
+(define (url-path-from-url url)
+  (string-join (map (lambda(x) (path/param-path x)) (url-path (string->url url))) "\\"))
 
-(define (calculate-local-location res save-location save-sublocation)
-  (let ([file-location (if ( equal? (url-host (string->url res))
-                                    (string-append 
-                                     (url-host (string->url res))
-                                     (string-join (map (lambda(x) (path/param-path x)) (url-path (string->url res))) "/"))
-                            )
-                             (build-path
-                                     (url-host (string->url res)) "index.html")
-                            (build-path 
-                                     (url-host (string->url res))
-                                     (string-join (map (lambda(x) (path/param-path x)) (url-path (string->url res))) "/")))]
-       )
-    (if (not(null? save-sublocation))
-        (begin
-          (display (string-append save-location "\n"))
-        (path->string (build-path save-location save-sublocation 
-                                  file-location))
-        )
-        (path->string(build-path save-location 
-                                 file-location)))))
+(define (path-from-url url)
+  (if(equal? "" (url-path-from-url url))
+     (build-path (url-host (string->url url)))
+     (build-path (url-host (string->url url)) (url-path-from-url url))))
 
-(define (save-resources! resources save-location save-sublocation)
-  (let ((res (car resources)))
+
+(define (calculate-local-location res save-location)
+  (let ([file-location (if ( equal? (url-host (string->url res)) (path->string (path-from-url res)))
+                           (build-path
+                            (url-host (string->url res)) "index.html")
+                           (path-from-url res))])
     (begin
-      (save! res save-location save-sublocation)
-      (save-resources! (cdr resources) save-location save-sublocation))))
+      (display (string-append save-location "\n"))
+      (display (string-append res "\n"))
+      (path->string (build-path save-location  
+                                file-location)))))
+
+(define (save-resources! resources save-location )
+  (if (null? resources) #f
+      (let ((res (car resources)))
+        (begin
+          (and (not (set-member? visited-hrefs res))
+               (begin
+                 (save! res save-location)
+                 (set! visited-hrefs (set-add visited-hrefs res))))
+          (save-resources! (cdr resources) save-location )))))
 
 (define (validate start-url depth save-location)
   (if (or (null? start-url) (null? depth) (null? save-location)) (list #f "Моля попълнете всички полета")
@@ -154,5 +155,5 @@
   (let ((valid (validate start-url depth save-location)))
     (and
      (car valid)
-        (crawl start-url depth save-location)
-        )))
+     (crawl start-url depth save-location)
+     )))
